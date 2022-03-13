2024adcef37e0aa4251d8d3a318f7a7270f0da9b
Ilya Goman <realtt@mail.ru>
2022-03-13 14:33:09 +0300

    introduce objects

---
+++ learn.js/Hello JS/src/objects.js
@@ -0,0 +1,32 @@
+let user = {
+    name: "Ilya",
+    age: 36
+}
+alert(user.name);
+
+user.isAdmin = true;
+
+alert(user.isAdmin);
+
+for (const key in user) {
+    if (Object.hasOwnProperty.call(user, key)) {
+        alert(`Property ${key} : ${user[key]}`);        
+    }
+}
+
+let schedule = {};
+
+alert( isEmpty(schedule) ); // true
+
+schedule["8:30"] = "get up";
+
+alert( isEmpty(schedule) ); // false
+
+function isEmpty(obj) {
+    let cnt = 0;
+    for (const key in obj) {
+        return true;
+    }
+
+    return false;
+}
