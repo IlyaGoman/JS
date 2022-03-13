be97da7831889fc5629c8d1bc17fb8d424e78a2a
Ilya Goman <realtt@mail.ru>
2022-03-13 13:26:09 +0300

    added TDD test

---
+++ learn.js/Hello JS/src/test.js
@@ -0,0 +1,10 @@
+describe("pow", function(){
+
+    it("2 в 3 степени будет 8", function(){
+        assert.equal(pow(2, 3), 8);
+    });
+
+    it("2 в 4 степени будет 16", function(){
+        assert.equal(pow(2, 4), 16);
+    });
+});
