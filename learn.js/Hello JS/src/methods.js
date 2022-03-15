e5e879dab051529ee15fdc8dc05ce4f727d2ddb3
goman_iv <realtt@mail.ru>
2022-03-15 09:56:47 +0300

    setting eslint and prettier

---
+++ learn.js/Hello JS/src/methods.js
@@ -0,0 +1,17 @@
+// user = {
+//     name: "Ilya",
+//     age: 36
+// };
+
+// user.sayHi = function () {
+//     alert( "Hi" );
+
+user = {
+    name: "Ilya",
+    age: 36,
+    sayHi() {
+        alert("Hi");
+    },
+};
+
+user.sayHi();
