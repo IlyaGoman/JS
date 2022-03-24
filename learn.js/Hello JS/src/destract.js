c28a81cdfaf0cda93f868a27e1804f6c9b601467
goman_iv <realtt@mail.ru>
2022-03-24 09:33:43 +0300

    destract

---
+++ learn.js/Hello JS/src/destract.js
@@ -0,0 +1,20 @@
+let salaries = {
+    "Jonh": 100,
+    "Pete": 300,
+    "Mary": 250
+};
+
+function topSalary(salaries) {
+    let max = 0;
+    let name = null;
+
+    for (const [key,value] of Object.entries(salaries)) {
+        if (value >= max) {
+            max = value;
+            name = key;
+        }
+    }
+    return name;
+}
+
+alert(topSalary(salaries));
